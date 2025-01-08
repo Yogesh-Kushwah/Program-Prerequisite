7f00fea8c5285f757e69e1301fd6a84c63621174
Yogesh Kushwah <YogeshKushwah775@gmail.com>
2025-01-08 21:54:46 +0530

    Add Two Numbers

---
+++ AddTwoNo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AddTwoNo{
+
+public static void main(String args[])
+{
+
+Scanner Sc=new Scanner(System.in);
+
+int num1= Sc.nextInt();
+int num2=Sc.nextInt();
+
+ System.out.println("The sum of numbers is: "+ (num1+num2));
+}
+}
